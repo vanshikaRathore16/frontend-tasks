6931858f9e55a9362204f7e42a293823f9845193
vanshikaRathore16 <vanshikarathore57@gmail.com>
2025-10-07 13:48:45 +0530

    adding text1.js

---
+++ text1.js
@@ -0,0 +1,66 @@
+// config command
+//     The git config command in Git is used to set, get, and list configuration options
+// to set global-------
+// git config --global user.name
+// git config --global user.emal
+// to set local---------
+// git config  user.name ""
+// git config  user.email ""
+// git config user.name 
+// git config user.email 
+
+// clone the repo
+// git clone https://github.com/vanshikaRathore16/temprary
+
+// branch list
+//    git branch
+// creeate branch
+//    git branch button-update
+//    git checkout -b branch indentation
+
+// add all change
+//  git add .
+
+ // to commit change
+//  git commit -m "chnage the indentation"
+
+ // to get last commite msg and after that we chnage it
+//  git reset --soft head~1
+
+ // tolaunch the graphical repository browser, :
+//  git gitk
+
+// to push code in github
+// git push -u origin indentation
+
+//stash 
+// it stash is a Git command used to temporarily save changes in your working directory and index (staged changes) that are not yet ready to be committed
+
+// git stash
+// git stash list
+// git  stash apply stash@{1}
+// (with this we can  show chnage in vs code)
+// git stash pop stash@{2}
+// error: Your local changes to the following files would be overwritten by merge:
+// ndex.html
+// Please commit your changes or stash them before you merge.
+// git stash drop stash@{2}
+
+
+// git init
+// git status
+// git remote add origin ""
+// git remote -v
+// git add .
+// git commit -m "msg"
+// git push -u origin main
+
+// git merge ui-update
+
+
+
+
+
+
+
+
